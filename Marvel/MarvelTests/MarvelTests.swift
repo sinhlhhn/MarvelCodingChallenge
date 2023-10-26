f405920fdb3ab8a8078d20c6e99bb39405953523
sinhlh <sinhlhhn@gmail.com>
2023-10-26 12:11:27 +0700

    init

---
+++ Marvel/MarvelTests/MarvelTests.swift
@@ -0,0 +1,14 @@
+//
+//  MarvelTests.swift
+//  MarvelTests
+//
+//  Created by Sam on 26/10/2023.
+//
+
+import XCTest
+
+class MarvelTests: XCTestCase {
+    func test_init() {
+        XCTFail("init")
+    }
+}
