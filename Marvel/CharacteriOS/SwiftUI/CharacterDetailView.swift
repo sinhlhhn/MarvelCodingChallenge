91f357c14c3787c61dc7bd6c9fb60552ed0e90ba
sinhlh <sinhlhhn@gmail.com>
2023-10-27 21:40:22 +0700

    move `CharacterDetailViewModel` to its own file

--- Marvel/CharacteriOS/SwiftUI/CharacterDetailView.swift
+++ Marvel/CharacteriOS/SwiftUI/CharacterDetailView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import Marvel
 
 public struct CharacterDetailView: View {
     
@@ -39,43 +38,6 @@ public struct CharacterDetailView: View {
             }
         }.onAppear {
             viewModel.loadData()
-        }
-    }
-}
-
-public class CharacterDetailViewModel: ObservableObject {
-    private let client: HTTPClient
-    private let url: URL
-    
-    enum State {
-        case loading
-        case success(CharacterDetailItem)
-        case failure(String)
-    }
-    
-    @Published var state: State = .loading
-    
-    public init(url: URL, client: HTTPClient) {
-        self.client = client
-        self.url = url
-    }
-    
-    func loadData() {
-        state = .loading
-        client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            DispatchQueue.main.async {
-                switch result {
-                case let .success((data, response)):
-                    do {
-                        self?.state = .success(try CharacterDetailMapper.map(data, response))
-                    } catch {
-                        self?.state = .failure(error.localizedDescription)
-                    }
-                case let .failure(error):
-                    self?.state = .failure(error.localizedDescription)
-                }
-            }
         }
     }
 }
---
+++ Marvel/CharacteriOS/SwiftUI/CharacterDetailViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  CharacterDetailViewModel.swift
+//  CharacteriOS
+//
+//  Created by Sam on 27/10/2023.
+//
+
+import SwiftUI
+import Marvel
+
+public class CharacterDetailViewModel: ObservableObject {
+    private let client: HTTPClient
+    private let url: URL
+    
+    enum State {
+        case loading
+        case success(CharacterDetailItem)
+        case failure(String)
+    }
+    
+    @Published var state: State = .loading
+    
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    func loadData() {
+        state = .loading
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            DispatchQueue.main.async {
+                switch result {
+                case let .success((data, response)):
+                    do {
+                        self?.state = .success(try CharacterDetailMapper.map(data, response))
+                    } catch {
+                        self?.state = .failure(error.localizedDescription)
+                    }
+                case let .failure(error):
+                    self?.state = .failure(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
