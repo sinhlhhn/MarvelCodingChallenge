5f374c2cc522268daebf29fe33a2b099b8a85257
sinhlh <sinhlhhn@gmail.com>
2023-10-27 16:12:51 +0700

    create CharacterRefreshViewController

---
+++ Marvel/CharacteriOS/Controllers/CharacterRefreshViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CharacterRefreshViewController.swift
+//  CharacteriOS
+//
+//  Created by Sam on 27/10/2023.
+//
+
+import UIKit
+import Marvel
+
+public final class CharacterRefreshViewController: NSObject {
+    private(set) lazy var view: UIRefreshControl = {
+        let view = UIRefreshControl()
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
+    }()
+
+    private let characterLoader: CharacterLoader
+
+    init(characterLoader: CharacterLoader) {
+        self.characterLoader = characterLoader
+    }
+
+    var onRefresh: (([CharacterItem]) -> Void)?
+
+    @objc func refresh() {
+        view.beginRefreshing()
+        characterLoader.load { [weak self] result in
+            DispatchQueue.main.async {
+                if let items = try? result.get() {
+                    self?.onRefresh?(items)
+                }
+                self?.view.endRefreshing()
+            }
+        }
+    }
+}
